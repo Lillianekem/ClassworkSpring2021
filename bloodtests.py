59b314536e1acbacd2aac45221269ff5f1aa57c2
Lillian <Lillian.ekem@duke.edu>
2021-01-28 16:18:05 -0500

    Created function to input and analyze HDL data

--- bloodtests.py
+++ bloodtests.py
@@ -6,5 +6,21 @@ def interface():
         choice = input("Enter an option:") 
         if choice == "9": 
             return 
-
-interface()
+            
+            
+def HDL_driver(): 
+    # Get data 
+    HDL_level = int(input("Enter HDL level:"))
+    # Analyze the data
+    if HDL_level >= 60: 
+        print("HDL level is normal") 
+    elif HDL_level < 60: 
+        if HDL_level > 40: 
+            print("HDL level is borderline low") 
+        else: 
+            print("HDL level is low")    
+    # Output the data 
+    return 
+    
+#interface()
+HDL_driver()
